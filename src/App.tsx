e9bc3ebb552ef3c2f3a40d2d089fff907a77f579
Ivo Bubeňko <ivobubenko@Ivos-MacBook-Air-2.local>
2026-02-12 20:34:31 +0100

    simple page example

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import { useState } from 'react'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Hello World!</h1>
-      <Box
-        component="form"
-        sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        <TextField id="filled-basic" label="Filled" variant="filled" />
-        <TextField id="standard-basic" label="Standard" variant="standard" />
-      </Box>
-    </>
+    <h1>Hello World!</h1>
   )
 }
 
